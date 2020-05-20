54b612760eb11a806c8397d6c1b2f86fe1abb4b0
Nuri Ra <nuri.ra@pix4d.com>
2020-05-20 21:12:52 +0200

    RAG-4829: Update mapbox URL

--- src/GeoFileTileCache.h
+++ src/GeoFileTileCache.h
@@ -8,12 +8,13 @@ class GeoFileTileCache : public QGeoFileTileCache
     Q_OBJECT
 
 public:
-    GeoFileTileCache(const QList<QGeoMapType>& mapTypes, int scaleFactor, const QString& directory = QString(), QObject* parent = nullptr);
+    GeoFileTileCache(const QList<QGeoMapType>& mapTypes, int scaleFactor, bool enableLogging, const QString& directory = QString(), QObject* parent = nullptr);
 
 protected:
     QString tileSpecToFilename(const QGeoTileSpec& spec, const QString& format, const QString& directory) const override;
     QGeoTileSpec filenameToTileSpec(const QString& filename) const override;
 
+    bool m_enableLogging{false};
     bool m_hasCacheDirectory{false};
     QList<QGeoMapType> m_mapTypes;
     QMap<QString, int> m_mapNameToId;
