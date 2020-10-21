1b19fef25cda5d0c09bb3eb40f8f8886bec921e0
Celia Pansard <celia.pansard@pix4d.com>
2020-10-21 17:29:01 +0200

    Test celia

--- conanfile.py
+++ conanfile.py
@@ -8,16 +8,35 @@ class QtBasemapPluginConan(ConanFile):
     license = 'LGPL3'
     url = 'http://code.qt.io/cgit/qt/qtlocation.git/tree/src/plugins/geoservices/mapbox?h=5.10'
     description = 'Qt GeoServices plugin for basemaps including MapBox'
-    options = {'shared': [True, False]}
-    default_options = 'shared=True'
+    options = {'shared': [True, False], 'lockfile': "ANY"}
+    default_options = {'shared': True, 'lockfile': None}
     settings = 'os', 'compiler', 'build_type', 'arch'
     generators = 'cmake'
-    exports_sources = ['CMakeLists.txt', 'src/*']
+    exports_sources = ['CMakeLists.txt', 'src/*', pix4d_build_tools/*',]
+    short_paths = True  # to circumvent Windows path length limit
+
+    def configure(self):
+        del self.settings.compiler.libcxx
+
+    def requirements(self):
+        if self.options.lockfile:
+            self.load_requirements_from_file(str(self.options.lockfile))
+        else:
+            self.requires('Qt5/[5.12.7]@pix4d/stable')
+ 
+    def load_requirements_from_file(self, path):
+        config = configparser.ConfigParser()
+        config.read(path)
+        for _, ref in config.defaults().items():
+            self.requires(ref)
 
     def build(self):
-        cmake = CMake(self, parallel=True)
+        cmake = CMake(self, generator='Ninja')
         cmake_args = {
-            '-DBUILD_SHARED_LIBS=': 'ON' if self.options.shared else 'OFF'
+            'VERSION_STRING': self.version,
+            'BUILD_SHARED_LIBS': self.options.shared,
+            'CMAKE_BUILD_TYPE': self.settings.build_type,
+            'CMAKE_INSTALL_PREFIX': self.package_folder,
         }
 
         if(tools.cross_building(self.settings) and
@@ -26,11 +45,17 @@ class QtBasemapPluginConan(ConanFile):
             # see https://github.com/conan-io/conan/issues/2856#issuecomment-421036768
             cmake.definitions["CONAN_LIBCXX"] = ""
 
-        cmake.configure(source_dir='..', build_dir='build', defs=cmake_args)
+        cmake.configure(source_dir='.', defs=cmake_args)
         cmake.build(target='install')
 
-    def configure(self):
-        del self.settings.compiler.libcxx
+    def package_id(self):
+        # Make all options and dependencies (direct and transitive) contribute
+        # to the package id
+        self.info.requires.full_package_mode()
+ 
+        # lockfile is just a way to select the version of the
+        # dependencies and it should not contribute to the package_id
+        del self.info.options.lockfile
 
     def package_info(self):
         self.cpp_info.libs = ["qtgeoservices_basemap_pix4d"]
