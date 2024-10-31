c6f8df4fbf7b486a587d6820f17f287d694c7420
Nuri Ra <nuri.ra@pix4d.com>
2024-10-31 18:05:55 +0100

    RAG-9416: Remove Mapbox map style URLs and access token, remove default Mapbox values, add streets_url and satellite_url to abstract QtBasemapPlugin"

--- src/qgeotilefetchermapbox.h
+++ src/qgeotilefetchermapbox.h
@@ -18,11 +18,9 @@ class QGeoTileFetcherMapbox : public QGeoTileFetcher
 
 public:
     // The list of map style names:
-    // The two values below are part of url allowing to fetch corresponsing mapbox tiles
-    static constexpr const char* PIX4D_STREET = "ck8zz9gpq0vty1ip30bji3b5a";
-    static constexpr const char* PIX4D_STREETS_SATELLITE = "ck8zzfxb30vwp1jo04yktjtbg";
-    // name for custom basemap tiles requests
-    static constexpr const char* PIX4D_CUSTOM = "custom";
+    static constexpr const char* PIX4D_STREETS = "Streets";
+    static constexpr const char* PIX4D_SATELLITE = "Satellite";
+    static constexpr const char* PIX4D_CUSTOM = "Custom";
 
 public:
     QGeoTileFetcherMapbox(int scaleFactor, bool enableLogging, const QString& customBasemapUrl, QGeoTiledMappingManagerEngine *parent);
@@ -30,7 +28,8 @@ public:
     void setUserAgent(const QByteArray &userAgent);
     void setMapIds(const QList<QString> &mapIds);
     void setFormat(const QString &format);
-    void setAccessToken(const QString &accessToken);
+    void setSatelliteUrl(const QString &satelliteUrl);
+    void setStreetsUrl(const QString &streetsUrl);
 
 private:
     QGeoTiledMapReply *getTileImage(const QGeoTileSpec &spec) override;
@@ -39,7 +38,8 @@ private:
     QByteArray m_userAgent;
     QString m_format;
     QString m_replyFormat;
-    QString m_accessToken;
+    QString m_satelliteUrl;
+    QString m_streetsUrl;
     QList<QString> m_mapIds;
     int m_scaleFactor;
     bool m_enableLogging{false};
